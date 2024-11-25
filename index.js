79100bc19f26a4f4fbfe8b518ab021f0a3c79710
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-11-25 16:31:59 +0100

    added shopped items

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import {menuArray} from "./data.js"
 const menu = document.getElementById("menu")
+const cart = document.getElementById("cart")
+const allItemsInCart = document.getElementById("all-items-in-cart")
 
 console.log(menuArray)
+
+let cartArr = []
 
 const displayMenu = (data) => {
     menu.innerHTML = ""
@@ -30,13 +34,25 @@ displayMenu(menuArray)
 document.addEventListener("click", (e) => {
     if (e.target.id === "buy-btn"){
         console.log(e.target.dataset.product)
-        displayCart(e.target.dataset.product)
+        updateCartArr(e.target.dataset.product)
+        displayCart(cartArr)
     }
 })
 
-const displayCart = (clickedProduct) => {
+const updateCartArr = (clickedProductId) => {
     const targetProduct = menuArray.filter((oneProduct) => {
-        return oneProduct.id === Number(clickedProduct)
+        return oneProduct.id === Number(clickedProductId)
     })[0]
     console.log(targetProduct)
+    cartArr.push(targetProduct)
+    console.log("cartArr",cartArr)
+    return cartArr
+}
+
+const displayCart = (cartArr) => {
+    cart.style.display = "flex"
+    const displaiedItemsInCart = cartArr.map((eachItem) =>{
+        return `<p>${eachItem.name}</p>`
+    }).join(" ")
+    return  allItemsInCart.innerHTML = displaiedItemsInCart
 }
