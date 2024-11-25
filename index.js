f74388a7d9d3d98998b13dbeb5563bc453149f9d
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-11-25 15:21:43 +0100

    det går inte så bra med sammanfattningen

--- index.js
+++ index.js
@@ -6,23 +6,34 @@ console.log(menuArray)
 const displayMenu = (data) => {
     menu.innerHTML = ""
     const allItems = data.map((oneItem) => {
-       const {name, ingredients, imgName, id, price} = oneItem
-    return `
-    <div class="menu-inner">
+        const {name, ingredients, imgName, id, price} = oneItem
+        return `
+        <div class="menu-inner" data-product=${id}>
         <img src="./images/${imgName}"/>
         <div class="food-details">
             <p>${name}</p>
             <p>${ingredients.map((oneIngredience) => {return oneIngredience}).join(", ")}</p>
             <p>${price} SEK</p>
         </div>
-        <button id="buy-btn" class="buy-btn"><img src="./images/add-btn.jpg"/></button>
-    </div>    
-    <div class="divider"></div>
+            <button id="buy-btn" class="buy-btn"><img src="./images/add-btn.jpg"/></button>
+        </div>    
+        <div class="divider"></div>
         `
         // <p>${([...ingredients])}</p>
-   }).join(" ")
-   menu.innerHTML = allItems
+    }).join(" ")
+    menu.innerHTML = allItems
 }
 
 displayMenu(menuArray) 
 
+const buyBtn = document.getElementById("buy-btn") 
+
+buyBtn.addEventListener("click", (e) => {
+    const productElement = e.target.parentElement.parentElement;
+    const productId = productElement.getAttribute("data-product")
+    console.log(productId )
+    const targetProduct = menuArray.filter((oneProduct) => {
+        return oneProduct.id === productId
+    })
+    console.log(targetProduct)
+})
